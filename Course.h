992a05de70c4b55a1fa9c1f165eba9047360ceb7
Sam Bostawros <sam.bostawros@student.giu-uni.de>
2025-11-11 09:59:42 +0200

    Course Commit Final

--- Course.h
+++ Course.h
@@ -4,19 +4,35 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Course{
+private:
+    string courseCode, courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course(){
+        courseCode = "Empty";
+        courseName = "Unknown";
+        maxStudents = 1;
+        students = new Student[maxStudents];
+        currentStudents = 0;
+    }
+    Course(string courseCode, string courseName, int maxStudents){
+        this->courseCode = courseCode;
+        this->courseName = courseName;
+        this->maxStudents = maxStudents;
+        students = new Student[maxStudents];
+        currentStudents = 0;
+    }
 
+    void addStudent(const Student& s);
+    void displayCourseInfo();
 
-
-
-
-
-
-
-
-
-
-
+    ~Course(){
+        delete students;
+    }
+};
 
 #endif
