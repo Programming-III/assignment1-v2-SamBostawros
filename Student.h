c30cfb706c24c536095194850122fb0403358cb8
Sam Bostawros <sam.bostawros@student.giu-uni.de>
2025-11-11 09:19:02 +0200

    Completed Class
    Refactored Student class to include constructors and member variables.

--- Student.h
+++ Student.h
@@ -3,19 +3,29 @@
 
 #include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
 
+class Student : public Person{
+private:
+    int yearLevel;
+    string major;
+public:
+    Student(){
+        this ->setName("No Name");
+        this ->setID(-1);
+        yearLevel = 0;
+        major = "Unknown";
+    }
+    Student(string name, int id, int yearLevel, string major){
+        this ->setName(name);
+        this ->setID(id);
+        this ->yearLevel = yearLevel;
+        this ->major = major;
+    }
+    void display();
+    ~Student(){
 
-
-
-
-
-
-
-
-
-
-
+    }
+};
 
 #endif
