fc8364b85679a31828a18e6c438590a7bf6de8b0
Sam Bostawros <sam.bostawros@student.giu-uni.de>
2025-11-11 09:19:25 +0200

    Completed Class

--- Instructor.h
+++ Instructor.h
@@ -3,17 +3,28 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
 
+class Instructor : public Person{
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor(){
+        this ->setName("No Name");
+        this ->setID(-1);
+        department = "Unknown";
+        experienceYears = -1;
+    }
+    Instructor(string name, int id, string department, int experienceYears){
+        this ->setName(name);
+        this ->setID(id);
+        this ->department = department;
+        this ->experienceYears = experienceYears;
+    }
+    void display();
+    ~Instructor(){
 
-
-
-
-
-
-
-
-
-
+    }
+};
 
 #endif
