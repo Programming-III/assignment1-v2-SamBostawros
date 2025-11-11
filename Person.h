9d91f84f5fe8cc2476fc3b9061a24c41d63e0319
Sam Bostawros <sam.bostawros@student.giu-uni.de>
2025-11-11 09:18:39 +0200

    Completed Class

--- Person.h
+++ Person.h
@@ -4,17 +4,31 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person{
+private:
+    string name;
+    int id;
+protected:
+    void setName(string name){
+        this -> name = name;
+    }
+    void setID(int id){
+        this -> id = id;
+    }
+public:
+    Person(){
+        name = "No Name";
+        id = -1;
+    }
+    Person(string name, int id){
+        this -> name = name;
+        this -> id = id;
+    }
+    void display();
+    ~Person(){
 
-
-
-
-
-
-
-
-
-
+    }
+};
 
 
 #endif
